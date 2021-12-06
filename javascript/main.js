8f81e922357979fedb7e41bf3b86fb551d208e69
Steffanie Eklind <steffanie.eklind@gmail.com>
2021-12-06 10:20:26 +0100

    Hämtar random bilder från collection

---
+++ javascript/main.js
@@ -0,0 +1,19 @@
+/*---------------------------Unsplash API Image-------------------------------------------------*/
+
+const numImagesAvailable = 258  //how many photos are total in the collection
+const numItemsToGenerate = 1; //how many photos you want to display
+const collectionID = 460091  //Collection ID
+const ImageContainer = document.querySelector(".image-container")
+
+
+function renderGalleryItem(randomNumber){
+  fetch(`https://source.unsplash.com/collection/${collectionID}/?sig=${randomNumber}`)
+    .then((response) => {
+        ImageContainer.innerHTML = `<img class="gallery-image" src="${response.url}" alt="gallery image"/>`
+  })
+}
+
+for(let i=0; i < numItemsToGenerate; i++){
+    let randomImageIndex = Math.floor(Math.random() * numImagesAvailable);
+    renderGalleryItem(randomImageIndex);
+}
