f0a752d25302016f669420e7088f0e5bb3bca264
shnam87 <shahramnamjo2000@gmail.com>
2021-12-17 17:16:48 +0100

    Rättade till ID-numreringen och gjorde några andra justeringar

--- javascript/localStorageToProductPage.js
+++ javascript/localStorageToProductPage.js
@@ -1,46 +1,44 @@
 let productListIndex = localStorage.length -1;
 let prodID = "product_" + productListIndex ;
-let id= 0;
+let id= 2;
 console.log(prodID);
 
 const productString= localStorage.getItem(prodID);
 const products = JSON.parse(productString)
-
 
 const products_Element = document.querySelector(".product-items");
 
 function displayInfo() {
     products.forEach( (product) => {
         products_Element.innerHTML += `
-        
-        <div class="product">
-            <div class="product-content">
-                <div class="product-img">
-                    <img src="${product.prodImg}" alt="${product.name}">
+      
+            <div class="product">
+                <div class="product-content">
+                    <div class="product-img">
+                        <img src="${product.prodImg}" alt="${product.name}">
+                    </div>
+                    <input type="hidden" id="${id++}">
+                    <button type="button" class="btn-wish">
+                        <i class="fa fa-heart-o" aria-hidden="true"></i>
+                    </button>
+                    <div class="product-btns">
+                        <button type="button" class="btn-cart">Lägg till i varukorg</button>
+                    </div>
                 </div>
-                <div class="produkt-btns">
-                    <button type="button" class="btn-wish"><i class="fa fa-heart-o" aria-hidden="true"></i></button>
-                    <button type="button" class="btn-cart">Lägg till i varukorg</button>
-                    <input type="hidden" value="${id++}">
-                    <button type="button" class="btn-buy">Köp nu</button>
+                <div class="product-info">
+                    <a href="#" class="product-name">${product.name}</a>
+                    <div class="product-info-top">
+                        <h2 class="sm-title">${product.description}</h2>
+                    </div>
+                    <p class="product-price">${product.price} </p>
+                    <p class="product-price">${product.price * 0.75}</p>
+                </div>
+                <div class="off-info">
+                    <h2 class="sm-title">25% rabatt</h2>
                 </div>
             </div>
-            <div class="product-info">
-                <div class="product-info-top">
-                    <h2 class="sm-title">${product.description}</h2>
-                </div>
-                <a href="#" class="product-name">${product.name}</a>
-                <p class="product-price">${product.price * 1.25 } </p>
-                <p class="product-price">${product.price}</p>
-            </div>
-            <div class="off-info">
-            <h2 class="sm-title">25% rabatt</h2>
-        </div>
-    </div>` ;
-
-        
+        `;
     });
-    
 }
 
 displayInfo();
