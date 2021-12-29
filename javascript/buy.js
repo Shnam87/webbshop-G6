9a1466a64d0d8211905772749d7dcc48115c146b
shnam87 <shahramnamjo2000@gmail.com>
2021-12-29 03:06:01 +0100

    fixade Orderbekräftelse

---
+++ javascript/buy.js
@@ -0,0 +1,9 @@
+document.querySelector("#buy").addEventListener("click", openModal);
+
+function openModal(e) {
+    // e.preventDefault();
+     
+     var modal = document.getElementById("buyModal");
+     modal.style.display = "block";
+ }
+ 
