d5a1c345999647f579799dfa9d06bcbf6067256a
lisa <lisasoders@gmail.com>
2021-12-30 14:29:20 +0100

    lagt till kommentarer

--- javascript/varukorg.js
+++ javascript/varukorg.js
@@ -1,26 +1,25 @@
 
-
-var varukorgProdukter = JSON.parse (localStorage.getItem("myCart")) || [];
 let addToCartBtn = document.querySelectorAll(".btn-cart");
+var varukorgProdukter = JSON.parse (localStorage.getItem("myCart")) || [];
 
 
-
+// loopar igenom alla knappar
 for(let i = 0; i < addToCartBtn.length; i++) {
     addToCartBtn[i].addEventListener('click', addToCart)
 }
 
 console.log(addToCartBtn)
 
+
+//funktion för att lägga till i varukorg
 function addToCart(event) {
-   let btn = event.target
+   let btn = event.target // för att kunna hämta ett visst element
    let btn_parent = btn.parentElement
    let btn_parentparent = btn_parent.parentElement
-
-   //let btn_grandparent = btn.parentElement.parentElement
    let itemName = btn_parent.children[1].innerText
    let itemPrice = btn_parent.children[3].innerText
    var prodImg = btn_parentparent.children[0].children[0].children[0].src
-  //let prodImg = localStorage.getItem("objImg");
+  
   console.log(prodImg);
 
   let newProduct = {
@@ -29,19 +28,17 @@ function addToCart(event) {
       prodImg:prodImg
   }
 
-  varukorgProdukter.push(newProduct)
-  localStorage.setItem("myCart", JSON.stringify(varukorgProdukter));
-  //localStorage.setItem("myCart", JSON.stringify(productLista));
+  varukorgProdukter.push(newProduct) // objektet newProduct pushas in i variabeln varukorgProdukter
+  localStorage.setItem("myCart", JSON.stringify(varukorgProdukter));  // setItem för att 'varukorgProdukter' ska hamna i 'myCart' i localStorage
+  
 addnum();
 
 }
 
-
+//funktion för att visa antal produkter som ligger i varukorgen
 function addnum () {
 const list = JSON.parse(localStorage.getItem("myCart"))
     document.querySelector(".cart-icon div").textContent = list.length 
-
-   
 
 }
 
